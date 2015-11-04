9a07dd1727c0b1434f6951b533f0de3618d681b0
murcielago17 <murcielago17@hotmail.com>
2015-11-03 23:55:44 -0800

    attempt 1 to get artist name from soptiy... still  not understanding wht to do with the data and where to allocta it

--- main.js
+++ main.js
@@ -5,5 +5,26 @@ $(function() {
   console.log('JS is loaded!');
   
   // your code here
+	$('#search').on('submit', function (event) {
+    
+    event.preventDefault();
+    console.log('form submitted!');
+
+    	var searchedArtist = $('#artist').val();
+    		console.log(searchedArtist);
+
+    	$.get('https://api.spotify.com/v1/search?q=' + searchedArtist + '&type=artist', function (data) {
+      
+      		console.log(data);
+    var artistResults = data.artists.items;
+
+      
+      artistResults.forEach(function (artist) {
+        console.log(artist.name);
+
+        
+        $('#results').append('<p>' + artist.name + '</p>');
+
 
 });
+
